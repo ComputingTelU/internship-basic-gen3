9178e7425d46d878a30d9bfd27fe228c984c1944
Wisnu Adi Nurcahyo <wisnu@nurcahyo.me>
2017-02-21 21:12:30 +0700

    Add answer to BigRandom

--- bigrandom/answer.py
+++ bigrandom/answer.py
@@ -1,3 +1,5 @@
+import math
+
 class BigRandom:
     def __init__(self):
         self.data = "data.txt"
@@ -10,7 +12,29 @@ class BigRandom:
                 # ommiting line number and its hashtag
 
         # your algorithm
+        datas = open(self.data, "r")
+        i    = 0
 
+        for line in datas:
+            if i < 10:
+                dex = 2
+            else:
+                dex = math.floor(math.log10(i)) + 2
+                
+            line = line[dex:]
+            noh += sum(1 for x in line if x == '#')
+            suc += sum(ord(x) for x in line)
+
+            i   += 1
+
+        datas.close()
         return (noh,suc)
 
     # add methods if you need more
+
+
+br       = BigRandom()
+noh, suc = br.answer()
+
+print("Sum of Hashtag   : " + str(noh))
+print("Sum of all ASCII : " + str(suc))
