3d7d87bddd09aab7fdcd9715196de4c2d7fc5e55
wikankun <wikan25@gmail.com>
2017-02-22 08:00:51 +0700

    first commit

--- bigrandom/answer.py
+++ bigrandom/answer.py
@@ -1,16 +1,20 @@
-class BigRandom:
-    def __init__(self):
-        self.data = "data.txt"
-        # add attributes if you need more
+def answer():
+    noh = 0 # variable to store number of hashtag
+            # ommiting line number's hashtag
+    suc = 0 # variable to store sum of character's code in ascii,
+            # ommiting line number and its hashtag
+    data = open('data.txt', 'r')
+    l = data.readline()
+    # your algorithm
+    for line in data:
+        for ch in line:
+            if ch == '#':
+                noh += 1
+        noh -= 1
+    suc = sum(ord(ch) for ch in data)
+    data.close()
+    print(int(noh))
+    print(int(suc))
+    return (noh,suc)
 
-    def answer(self):
-        noh = 0 # variable to store number of hashtag
-                # ommiting line number's hashtag
-        suc = 0 # variable to store sum of character's code in ascii,
-                # ommiting line number and its hashtag
-
-        # your algorithm
-
-        return (noh,suc)
-
-    # add methods if you need more
+answer()
