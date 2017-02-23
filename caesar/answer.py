f0f87965695dc9a07065548759525707fd78928a
girhoz <giasirham@gmail.com>
2017-02-24 00:30:07 +0700

    Internship Lalu Gias Irham

--- caesar/answer.py
+++ caesar/answer.py
@@ -7,7 +7,17 @@ class Caesar:
         key = 0 # variable to store the key
 
         # your algorithm
+        data = open(chiper.data, "r")
+        Ascii = range(128)
 
+
+        data.close()
         return (key)
 
     # add methods if you need more
+
+
+chiper = Caesar()
+ans = chiper.answer()
+
+print ("Key to encrypt and decrypt chypertext: ", ans)
