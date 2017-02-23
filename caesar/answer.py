9d122bc24de8babd01265d2df3889af8f1cfca59
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-02-24 05:32:50 +0700

    Caesar

--- caesar/answer.py
+++ caesar/answer.py
@@ -7,7 +7,27 @@ class Caesar:
         key = 0 # variable to store the key
 
         # your algorithm
-
+        f = open(self.ciphertext, "r")
+        text="Tugas"
+        for line in f:
+            cond = False
+            key = 1
+            while not(cond):
+                dummy= line
+                x=[]
+                for each in dummy:
+                    x.append(each)
+                for i in range(len(x)):
+                    x[i] = chr((int(ord(x[i])) + key) % 128)
+                dummy= "".join(x)
+                if text in dummy:
+                    cond=True
+                    #buat nge cek hasil dari decode nya
+                    #print(dummy)
+                else:
+                    key+=1
         return (key)
-
     # add methods if you need more
+x= Caesar()
+print(x.answer())
+#jawaban nya ga ada yang benar, 81 buat tulisan dan kata kata benar, 82 buat spasi enter dan angka angka
