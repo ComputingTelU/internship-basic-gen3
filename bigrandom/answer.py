47b94dfe0efe3554bf8b694e05dad4662310ec04
Vatana Rianti Aldefi <Vatana Rianti Aldefi>
2017-02-24 20:51:29 +0700

    ya sudahlah

--- bigrandom/answer.py
+++ bigrandom/answer.py
@@ -8,9 +8,19 @@ class BigRandom:
                 # ommiting line number's hashtag
         suc = 0 # variable to store sum of character's code in ascii,
                 # ommiting line number and its hashtag
-
         # your algorithm
-
+        data = open ("data.txt", "r")
+        for char in data :
+            while (char[0] != "#") :
+                char = char[1:]
+            char = char[1:]
+            for i in char :
+                if (i == "#") :
+                    noh+=1
+                suc = suc + ord(char)
+        data.close()
         return (noh,suc)
 
     # add methods if you need more
+data = BigRandom()
+print(data.answer())
