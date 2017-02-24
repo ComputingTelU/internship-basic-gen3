39986247d25c528baf74c83236ca01d3b2cf122f
yogawidi <yogawidi1@gmail.com>
2017-02-24 19:18:20 +0700

    upload tugas yoga

--- caesar/answer.py
+++ caesar/answer.py
@@ -11,3 +11,22 @@ class Caesar:
         return (key)
 
     # add methods if you need more
+        word = open("ciphertext.txt,","r")
+        text = open("apagitu.txt","w");
+        i=0
+        new=""
+        for x in word.readline():
+            i=0
+            i=ord(str(x))-46
+            if(i<0):
+                i=i+127
+            new += chr(i)
+        text.write(new)
+        print(new)
+        text.close()
+        word = open("ciphertext.txt","r")
+        import collections
+        s = word.readline()
+        print(ord(str(collections.Counter(s).most_common(1))))
+ans = Caesar()
+print (ans.answer())
