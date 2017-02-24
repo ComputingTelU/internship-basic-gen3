62ef7ffe0fda03869d122500784190bbcee43f1e
yogawidi <yogawidi1@gmail.com>
2017-02-24 11:14:13 +0700

    upload tugas yoga

---
+++ bigrandom/answer.py
@@ -0,0 +1,41 @@
+class BigRandom:
+    def __init__(self):
+        self.data = "data.txt"
+        # add attributes if you need more
+
+    def answer(self):
+        noh = 0 # variable to store number of hashtag
+                # ommiting line number's hashtag
+        suc = 0 # variable to store sum of character's code in ascii,
+                # ommiting line number and its hashtag
+        data = open('data.txt','r')
+        # your algorithm
+        line = ""
+        for char in data:
+
+            for x in range(0,len(char)):
+                if (char[x]=='#'):
+                    line = char[x+1:]
+                    break
+                x+=1
+            for y in line:
+                if (y=='#'):
+                    noh+=1
+                suc+=ord(y)
+
+            # i=0
+            # while (line[0]!="#"):
+
+        # for char in data.readline():
+        #     if (char=='#'):
+        #         noh+=1
+        #     else:
+        #         suc= suc+ ord(char)
+        #
+        # noh = noh-10000
+        # suc = suc-orc(10000)
+        return (noh,suc)
+
+    # add methods if you need more
+big = BigRandom()
+print (big.answer())
