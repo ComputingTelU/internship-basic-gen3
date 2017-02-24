ecce9ded2b09d64b4687f3c8a7929d85a102b8ff
nafasamuth <nafasa.muth@outlook.com>
2017-02-24 14:32:27 +0800

    Nafasa

--- bigrandom/answer.py
+++ bigrandom/answer.py
@@ -3,6 +3,7 @@ class BigRandom:
         self.data = open("data.txt", "r")
         # add attributes if you need more
 
+    @property
     def answer(self):
         noh = 0 # variable to store number of hashtag
                 # ommiting line number's hashtag
@@ -15,14 +16,15 @@ class BigRandom:
             for x in c:
                 c = c[1:]
                 if (x == "#"):
-                    noh += 1
                     break
             for x in c:
+                if (x == "#"):
+                    noh += 1
                 suc += ord(x)
         return (noh,suc)
 
 
 b = BigRandom()
-print (b.answer())
+print (b.answer)
 
     # add methods if you need more
--- caesar/answer.py
+++ caesar/answer.py
@@ -1,13 +1,25 @@
 class Caesar:
     def __init__(self):
-        self.ciphertext = "ciphertext.txt"
+        self.ciphertext = open("ciphertext.txt","r")
         # add attributes if you need more
 
+    @property
     def answer(self):
         key = 0 # variable to store the key
 
         # your algorithm
-
-        return (key)
-
+        b = False
+        while not b:
+            to_find = "xxx"
+            a = []
+            for i in c.ciphertext:
+                d = chr(int((ord(i)+key)%127))
+                a.append(d)
+            c.f = "".join(a)
+            if to_find in c.ciphertext:
+                b = True
+                return (key)
+            key +=1
+c = Caesar()
+print (c.answer())
     # add methods if you need more
