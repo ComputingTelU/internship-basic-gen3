6fd58c97d8931bb551608a4e2f91c350137a09db
underground-11 <the.underground.eleven@gmail.com>
2017-02-24 09:08:34 +0700

    imdone
    Nama : Iqbal Basyar
    status :
    big random selesai - benar
    caesar selesai - gatau benar atau engga.

--- caesar/answer.py
+++ caesar/answer.py
@@ -2,12 +2,23 @@ class Caesar:
     def __init__(self):
         self.ciphertext = "ciphertext.txt"
         # add attributes if you need more
-
     def answer(self):
         key = 0 # variable to store the key
 
         # your algorithm
-
-        return (key)
-
+        sudah = False
+        while not sudah:
+            tofind = "tugas"
+            y = []
+            for i in ans.ciphertext:
+                w = chr(int((ord(i)+key)%127))
+                y.append(w)
+            ans.res = "".join(y)
+            if tofind in ans.res:
+                sudah = True
+                return (key)
+            key +=1
+            
+ans = Caesar()
+print (ans.answer())
     # add methods if you need more
