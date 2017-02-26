e18835144a42e395c3b144d0d23b4e3f6ca22344
Kukuh Aji Sulistyo <sulistyo.kukuh@gmail.com>
2017-02-26 22:21:23 +0700

    add casear/answer.py + minor change in bigrandom/answer.py

--- bigrandom/answer.py
+++ bigrandom/answer.py
@@ -1,16 +1,24 @@
 class BigRandom:
     def __init__(self):
-        self.data = open("data.txt", "r")
-        # add attributes if you need more
+    	self.data = "data.txt"
 
     def answer(self):
+        self.data = open("data.txt", "r")
         noh, suc = 0, 0
         for line in self.data.readlines(): 
-            noh = noh + len([x for x in line if x == '#']) # count seluruh '#'
-            suc = suc + sum([ord(x) for x in line]) # sum dari ascii seluruh karakter
-        nomor = ''.join([str(x) for x in range(10000)]) # 1234567891011121314...999910000
-        return (noh - 10000, suc - (ord('#') * 10000 + sum([ord(x) for x in nomor])))
+            noh = noh + len([x for x in line if x == '#']) #______ count of '#'
+            suc = suc + sum([ord(x) for x in line]) #_____________ sum of asciis of all chars
 
+           
+        numbs = ''.join([str(x) for x in range(10000)])	# 1234567891011121314...999910000
+
+        self.data.close() # close data stream
+
+        # ord('#') * 10000 _________________ sum of asciis of first '#' in each row
+        # sum([ord(x) for x in nomor]) _____ sum of asciis of numbs
+        return (noh - 10000, suc - (ord('#') * 10000 + sum([ord(x) for x in numbs])))
+
+# instantiation
 big = BigRandom()
 a, b = big.answer()
 print(a, b)
--- caesar/answer.py
+++ caesar/answer.py
@@ -4,10 +4,13 @@ class Caesar:
         # add attributes if you need more
 
     def answer(self):
-        key = 0 # variable to store the key
+        self.ciphertext = open("ciphertext.txt", "r+")
+        key = int(raw_input(":> ")) # _____________________________________________________ get key from user
+        msg = ''.join([chr(ord(x) - key) for x in str(self.ciphertext.readlines())]) # ____ decripting @return decripted string
+        print("Hasil decrypt dengan key %d adalah \n %s" % (key, msg)) # _________________________ printing out
+        self.ciphertext.close() # _________________________________________________________ close data stream
+        return (msg)
 
-        # your algorithm
-
-        return (key)
-
-    # add methods if you need more
+# instantiation
+csr = Caesar()
+print(csr.answer());
