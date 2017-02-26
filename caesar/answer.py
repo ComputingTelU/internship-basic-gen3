a24131887d7509571a70b3dd20f2e1ca93c98393
Kukuh Aji Sulistyo <sulistyo.kukuh@gmail.com>
2017-02-26 22:24:54 +0700

    minor change

--- caesar/answer.py
+++ caesar/answer.py
@@ -7,7 +7,7 @@ class Caesar:
         self.ciphertext = open("ciphertext.txt", "r+")
         key = int(raw_input(":> ")) # _____________________________________________________ get key from user
         msg = ''.join([chr(ord(x) - key) for x in str(self.ciphertext.readlines())]) # ____ decripting @return decripted string
-        print("Hasil decrypt dengan key %d adalah \n %s" % (key, msg)) # _________________________ printing out
+        print("Hasil decrypt dengan key %d adalah \n %s" % (key, msg)) # __________________ printing out
         self.ciphertext.close() # _________________________________________________________ close data stream
         return (msg)
 
