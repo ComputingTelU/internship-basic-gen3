2fdcd01add9e8bb6c8fbdaeefaf33c7cedaf20d2
Kukuh Aji Sulistyo <kamitetepde@gmail.com>
2017-02-26 21:07:16 +0700

    updating bigrandom/answer.py

--- bigrandom/answer.py
+++ bigrandom/answer.py
@@ -5,16 +5,10 @@ class BigRandom:
 
     def answer(self):
         noh, suc = 0, 0
-        nomor = ''.join([str(x) for x in range(10000)]) # 1234567891011121314...999910000
         for line in self.data.readlines(): 
             noh = noh + len([x for x in line if x == '#']) # count seluruh '#'
             suc = suc + sum([ord(x) for x in line]) # sum dari ascii seluruh karakter
-
-            # @return
-            #       noh = count suluruh '#' - 10000, karena '#' disetiap penomoran baris tidak dihitung
-
-            #       ord('#') * 10000             = sum ascii dari '#' untuk setiap penomoran baris
-            #       sum([ord(x) for x in nomor]) = sum ascii dari seluruh nomor 1234567891011121314...999910000
+        nomor = ''.join([str(x) for x in range(10000)]) # 1234567891011121314...999910000
         return (noh - 10000, suc - (ord('#') * 10000 + sum([ord(x) for x in nomor])))
 
 big = BigRandom()
