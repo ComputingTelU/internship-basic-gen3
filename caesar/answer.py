daf14c8ae57f756c9b02e990c5e19261884e353d
nafasamuth <nafasa.muth@outlook.com>
2017-02-26 20:55:15 +0800

    Nafasa

--- caesar/answer.py
+++ caesar/answer.py
@@ -1,25 +1,24 @@
 class Caesar:
     def __init__(self):
-        self.ciphertext = open("ciphertext.txt","r")
+        self.ciphertext = "ciphertext.txt"
         # add attributes if you need more
 
-    @property
     def answer(self):
         key = 0 # variable to store the key
 
         # your algorithm
-        b = False
-        while not b:
-            to_find = "xxx"
-            a = []
-            for i in c.ciphertext:
-                d = chr(int((ord(i)+key)%127))
-                a.append(d)
-            c.f = "".join(a)
-            if to_find in c.ciphertext:
-                b = True
-                return (key)
-            key +=1
+        a = open(self.ciphertext, "r")
+        b = ""
+        key = 46
+        n = 0
+        for x in a.read():
+            n = ord(str(x))- key
+            if (n<0):
+                n += 127
+            b += chr(n)
+        print(b)
+        return(key)
+
+    # add methods if you need more
 c = Caesar()
 print (c.answer())
-    # add methods if you need more
